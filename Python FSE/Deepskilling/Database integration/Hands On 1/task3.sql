3323d5aa8903ff4401a0eb77f14bc64c271fef3a
SanthiyaRajarao <144979216+SanthiyaRajarao@users.noreply.github.com>
2026-07-01 14:11:40 +0530

    comm
    This script alters the schema of multiple tables, adding new columns, constraints, and renaming a column. It also includes commands to describe the tables before and after changes.

--- Python FSE/Deepskilling/Database integration/Hands On 1/task3.sql
+++ Python FSE/Deepskilling/Database integration/Hands On 1/task3.sql
@@ -1,1 +1,27 @@
+-- TASK 3 : ALTER TABLE AND EXTEND SCHEMA
 
+ALTER TABLE students
+ADD phone_number VARCHAR(15);
+
+DESCRIBE students;
+
+ALTER TABLE courses
+ADD max_seats INT DEFAULT 60;
+
+DESC courses;
+
+ALTER TABLE enrollments
+ADD CONSTRAINT chk_grade
+CHECK (grade IN ('A','B','C','D','F') OR grade IS NULL);
+
+DESC enrollments;
+
+ALTER TABLE departments
+RENAME COLUMN hod_name TO head_of_dept;
+
+DESC departments;
+
+ALTER TABLE students
+DROP COLUMN phone_number;
+
+DESC students;
